5414520c5a77440997688379f1d564207f996329
Surya <suryab.2392000@gmail.com>
2022-08-09 11:39:06 +0530

    c

--- server.js
+++ server.js
@@ -696,7 +696,9 @@ app.get('/summary', async(req, res) => {
         console.log(error)
     }
 })
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'client/build/index.html'));
+} )
 app.listen(PORT, () =>{
     console.log("App Listening in ", PORT)
 })
